30ca7beea30dc584141f78ed4f2fef57b7ac59c4
kojimauchidakaikei-spec <kojima.uchidakaikei@gmail.com>
2026-07-31 10:33:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="rent-history-v2.1.0";
+const CACHE="rent-history-v2.1.1";
 const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
